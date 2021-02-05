f985698b9e3942d98a6a2ae683f0e0e4d9e800e0
rheadp <rheadpea@msu.edu>
2021-02-05 09:56:44 -0500

    commit tree 9142eb954d7f6853195b4902b5571c9d68f015f4

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
-def unsafe?(speed)
 
+def unsafe?(speed)
+    if speed < 40 || speed > 60 
+        return TRUE
+    else 
+        return FALSE
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40  || speed > 60 ? TRUE : FALSE
 end
-	
 
 
